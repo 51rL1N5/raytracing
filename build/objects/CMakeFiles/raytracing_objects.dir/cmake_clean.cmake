69e9f0f46d865ae061d8523bd941e1717f8b5a1e
Matheus <Matheus Oliveira>
2019-06-18 02:33:32 -0300

    adicionando reflexo

---
+++ build/objects/CMakeFiles/raytracing_objects.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracing_objects.dir/object.cpp.o"
+  "CMakeFiles/raytracing_objects.dir/sphere.cpp.o"
+  "CMakeFiles/raytracing_objects.dir/triangle.cpp.o"
+  "CMakeFiles/raytracing_objects.dir/cube.cpp.o"
+  "libraytracing_objects.pdb"
+  "libraytracing_objects.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracing_objects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
