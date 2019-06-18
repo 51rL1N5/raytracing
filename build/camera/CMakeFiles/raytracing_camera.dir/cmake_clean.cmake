ac874023db0c98ac070645aefdc5bc624265e5ef
Matheus <Matheus Oliveira>
2019-06-17 21:12:49 -0300

    consertando parcialmente raytracing

---
+++ build/camera/CMakeFiles/raytracing_camera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracing_camera.dir/camera.cpp.o"
+  "libraytracing_camera.pdb"
+  "libraytracing_camera.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracing_camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/objects/CMakeFiles/raytracing_objects.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracing_objects.dir/object.cpp.o"
+  "CMakeFiles/raytracing_objects.dir/sphere.cpp.o"
+  "libraytracing_objects.pdb"
+  "libraytracing_objects.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracing_objects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
